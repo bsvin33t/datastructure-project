b026ef1a091ae6036b2217023f35d0abd0d3c3cc
Vineeth Sathish <bsvin33t@gmail.com>
2019-10-14 12:10:01 +0200

    Update new.rb

--- new.rb
+++ new.rb
@@ -1,34 +1,30 @@
 puts"**********************************\n\tWelcome to candy store\n**********************************"
-	
-	puts"\nHow many candy Brand count u Want to add :"
-    brand=gets
-    brand=brand.to_i
-	puts"\n"
+
+puts"\nHow many candy Brand count u Want to add :"
+brand=gets
+brand=brand.to_i
+puts"\n"
 candystore_hash = {}
-	[1..brand].each.with_index() do |i|
-input = ""
-name = ""
-count = ""
+[1..brand].each do
+  input = ""
+  name = ""
+  count = ""
 
-puts "type hi or Press Enter to quit"
-input = gets.chomp
+  puts "Type quit to quit, to continue, press enter"
+  
+  input = gets.chomp
+  break if input == "quit"
+  
+  puts "Enter name of candy: "
+  name = gets.chomp
+  puts "Enter candy count: "
+  count = gets.chomp
 
 
-while input != "" do 
- puts "Enter name of candy: "
- name = gets.chomp
- puts "Enter candy count: "
- count = gets.chomp
-
-
- candystore_hash[name] = count
- input = gets.chomp
-end
-
-	
+  candystore_hash[name] = count
 end
 
 puts "Here is the content that was in the hash: "
 candystore_hash.each do |key, value|
- puts " #{key} => #{value}"
+  puts " #{key} => #{value}"
 end
