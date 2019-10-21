7a0d238b105c0287324811da31a2a8b43846580a
Akshatha <micedreams@gmail.com>
2019-10-21 12:56:32 +0530

    Add files via upload

---
+++ candystore.rb
@@ -0,0 +1,39 @@
+
+
+puts"\nHow many candy flavours count u Want to add :"
+flavour=gets
+flavour=flavour.to_i
+puts"\n"
+candystore_hash = {}
+(1..flavour).each do
+  input = ""
+  name = ""
+  count = ""
+
+  puts "Type quit to quit, press enter to continue"
+
+  input = gets.chomp
+  break if input == "quit"
+
+  puts "Enter name of candy: "
+  name = gets.chomp
+  puts "Enter candy count: "
+  count = gets.chomp
+
+
+  candystore_hash[name] = count
+end
+
+puts "Here is the content that was in the hash: "
+
+candystore_hash.each do |key, value|
+  puts " #{key} => #{value}"
+
+
+end
+ 
+a=candystore_hash.values
+puts"#{a}"
+ @sum = 0
+ @a.each_index { |i| @sum += @a[i] }
+ puts"#{sum}"
